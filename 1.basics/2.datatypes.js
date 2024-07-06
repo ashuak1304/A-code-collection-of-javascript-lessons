236c379fe9360dd9ddfc2eeb34f9ad79d9507d6c
ashuak1304 <sharmaashutosh93159@gmail.com>
2024-07-06 03:43:16 +0000

    datatypes

---
+++ 1.basics/2.datatypes.js
@@ -0,0 +1,10 @@
+"use strict" //now we can use the newer veraion of JS
+
+//readability is the most imp. thing
+//JS automatically removes the empty space
+
+alert("gyukfq")    //we are using node.js and not browser , it will give error
+
+
+console.log( 3+3);    
+console.log("ashutosh sharma");
